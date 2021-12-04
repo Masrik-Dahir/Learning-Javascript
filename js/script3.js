15a9c94f7ac007a0491965b755aabb4ee7f4f3d0
Masrik Dahir <dahirma@vcu.edu>
2021-12-04 13:41:54 -0500

    new

--- js/script3.js
+++ js/script3.js
@@ -6,19 +6,24 @@ const backpack = {
     name: "Everyday Backpack",
     volume: 30,
     color: "grey",
-    pocketNum: 15,
-    strapLength: {
+    n_pocket: 15,
+    l_strap: {
         left: 26,
         right: 26,
     },
-    lidOpen: false,
-    toggleLid: function (lidStatus) {
-        this.lidOpen = lidStatus;
+    lid_open: false,
+    toggle_lid: function (lid_status) {
+        this.lid_open = lid_status;
     },
-    newStrapLength: function (lengthLeft, lengthRight) {
-        this.strapLength.left = lengthLeft;
-        this.strapLength.right = lengthRight;
+    new_l_strap: function (l_left, l_right) {
+        this.l_strap.left = l_left;
+        this.l_strap.right = l_right;
     },
 };
 
-console.log("The backpack object: ", backpack)
+console.log("The backpack object: ", backpack);
+
+console.log("The n_pocket value: ",backpack.n_pocket);
+
+var query = "n_pocket";
+console.log("The n_pocket value: ",backpack[query]);
